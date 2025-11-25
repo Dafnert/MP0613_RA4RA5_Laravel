6e8adfc7aa6f8f7e04c93e5385c46e4fd0e584fd
Dafnert <dafne.rt04@gmail.com>
2025-11-25 18:48:52 +0100

    Fix #5 sort list by year

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,7 @@
         <li><a href=/filmout/films>Pelis</a></li>
         <li><a href=/filmout/yearFilms>Pelis por Año</a></li>
         <li><a href=/filmout/genreFilms>Pelis por Género</a></li>
+        <li><a href=/filmout/sortFilms>Pelis ordenadas por Año (nuevas a antinguas)</a></li>
     </ul>
     <!-- Add Bootstrap JS and Popper.js (required for Bootstrap) -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
